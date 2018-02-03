6d7aa61434cbe25de49f468ad3383d7e824ae52e
wantongtang <1048517841@qq.com>
2018-02-03 05:52:45 +0000

    test1

--- SliceAudio.py
+++ SliceAudio.py
@@ -10,6 +10,9 @@ import audioread
 import time
 
 
+from predict import  *
+
+
 
 """
 Dependencies:
@@ -132,6 +135,57 @@ def slice_audio(file, channels, outformat, width, rate, slice_length, slide):
 			backwards.export(ARGS.output_dir+fileName.split('/')[-1]+'backwards_slice'+str(slice_start/1000)+'SecsToEndFileAt'+str((slice_start+length_slice_ms)/1000)+'Secs.'+outformat, format=outformat)
 		#Save the sewn together backwards bits to file
 		notes_reversed.export(ARGS.output_dir+fileName.split('/')[-1]+'notes_reversed_granular.'+outformat, format=outformat)
+
+
+
+
+def slice_audio1(file, channels, outformat, width, rate, slice_length, slide):
+	outformat = outformat.replace('.','').lower()
+	#Allow the user to see their x-bit selection with this dictionary.
+	width_translator = {1:'8-bit', 2:'16-bit', 4:'32-bit'}
+	#For every file in the input list do processing.
+#	for file in files:
+	if True:
+		fileName, fileExtension = os.path.splitext(file)
+		#Print to screen the processing parameters.
+		with audioread.audio_open(file) as f:
+			print '\nConverting '+fileName+' from:'
+			print fileExtension+' to .'+outformat+';'
+			print str(f.channels)+' channel(s) to '+str(channels)+' channel(s);'
+			print str(f.samplerate)+' Hz to '+str(rate)+' Hz;'
+			print 'Slicing '+str(f.duration*1000)+' ms file into '+str(slice_length)+' ms slices with a window slide of '+str(slide)+' ms;'
+		#Store the file in RAM.
+		sound = AudioSegment.from_file(file, fileExtension.replace('.','').lower())
+		#Print the 'x-bit' conversion parameters.
+		print width_translator[sound.sample_width]+' to '+width_translator[int(width)]+'.\n'
+		#Implement the user-selected or default (if nothing selected) parameters for processing.
+		sound = sound.set_frame_rate(int(rate))
+		sound = sound.set_sample_width(int(width))
+		sound = sound.set_channels(int(channels))
+		length_sound_ms = len(sound)
+		length_slice_ms = int(slice_length)
+		slice_start = 0
+		#create audiosegment object
+		notes_reversed = sound[0:1].reverse()
+		#Begin slicing at the start of the file.
+		while slice_start + length_slice_ms < length_sound_ms:
+			sound_slice = sound[slice_start:slice_start+length_slice_ms]
+			backwards = sound_slice.reverse()
+			notes_reversed += backwards
+			sound_slice.export(ARGS.output_dir+fileName.split('/')[-1]+'.slice'+str(slice_start/1000)+'SecsTo'+str((slice_start+length_slice_ms)/1000)+'Secs.'+outformat, format=outformat)
+			backwards.export(ARGS.output_dir+fileName.split('/')[-1]+'backwards_slice'+str(slice_start/1000)+'SecsTo'+str((slice_start+length_slice_ms)/1000)+'Secs.'+outformat, format=outformat)
+			slice_start += int(slide)
+		#When the slice is abutting the end of the file, output that slice too.'
+		if slice_start + length_slice_ms >= length_sound_ms:
+			sound_slice = sound[slice_start:length_sound_ms]
+			backwards = sound_slice.reverse()
+			notes_reversed += backwards
+			sound_slice.export(ARGS.output_dir+fileName.split('/')[-1]+'.slice'+str(slice_start/1000)+'SecsToEndFileAt'+str((length_sound_ms)/1000)+'Secs.'+outformat, format=outformat)
+			backwards.export(ARGS.output_dir+fileName.split('/')[-1]+'backwards_slice'+str(slice_start/1000)+'SecsToEndFileAt'+str((slice_start+length_slice_ms)/1000)+'Secs.'+outformat, format=outformat)
+		#Save the sewn together backwards bits to file
+		notes_reversed.export(ARGS.output_dir+fileName.split('/')[-1]+'notes_reversed_granular.'+outformat, format=outformat)
+
+
 
 
 def main():
--- predict.py
+++ predict.py
@@ -44,6 +44,7 @@ from tensorflow.contrib.framework.python.ops import audio_ops as contrib_audio
 
 import csv
 
+
 SUBMIT=True
 FLAGS = None
 
@@ -111,6 +112,71 @@ def run_graph(wavdir, labels, input_layer_name, output_layer_name,
 
     return 0
 
+def run_graph1(wavfilename, labels, input_layer_name, output_layer_name,
+              num_top_predictions):
+  """Runs the audio data through the graph and prints predictions."""
+  with tf.Session() as sess:
+    # Feed the audio data as input to the graph.
+    #   predictions  will contain a two-dimensional array, where one
+    #   dimension represents the input image count, and the other has
+    #   predictions per class
+    softmax_tensor = sess.graph.get_tensor_by_name(output_layer_name)
+    if SUBMIT:
+      if 1:
+        if 1:
+          with open(wavfilename, 'rb') as wav_file:
+            wav_data = wav_file.read()
+            predictions, = sess.run(softmax_tensor, {input_layer_name: wav_data})
+
+    # Sort to show labels in order of confidence
+            top_k = predictions.argsort()[-num_top_predictions:][::-1]
+            for node_id in top_k:
+              human_string = labels[node_id]
+              score = predictions[node_id]
+              writer.writerow([alldir,human_string])
+              print('%s %s' % (alldir,human_string))
+    else:
+      with open("nosubmit.csv","w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["fname","label","score"])
+        for alldir in os.listdir(wavdir):
+          with open(os.path.join('%s%s' %(wavdir,alldir)), 'rb') as wav_file:
+            wav_data = wav_file.read()
+            predictions, = sess.run(softmax_tensor, {input_layer_name: wav_data})
+
+    # Sort to show labels in order of confidence
+            top_k = predictions.argsort()[-num_top_predictions:][::-1]
+            for node_id in top_k:
+              human_string = labels[node_id]
+              score = predictions[node_id]
+              writer.writerow([alldir,human_string,score])
+              print('%s %s %f' % (alldir,human_string,score))
+              if score >0.5:
+                os.system("cp -rf %s /tmp/speech_dataset/%s/%s"%(os.path.join('%s%s' %(wavdir,alldir)),human_string,alldir))
+                print("coppied,score = %f"%score)
+
+      
+
+
+    return 0
+
+def label_wav1(wavfilename, labels, graph, input_name, output_name, how_many_labels):
+  """Loads the model and labels, and runs the inference to print predictions."""
+#	if not wav or not tf.gfile.Exists(wav):
+#    tf.logging.fatal('Audio file does not exist %s', wav)
+
+  if not labels or not tf.gfile.Exists(labels):
+    tf.logging.fatal('Labels file does not exist %s', labels)
+
+  if not graph or not tf.gfile.Exists(graph):
+    tf.logging.fatal('Graph file does not exist %s', graph)
+
+  labels_list = load_labels(labels)
+
+  # load graph, which is stored in the default session
+  load_graph(graph)
+
+  run_graph1(wavfilename, labels_list, input_name, output_name, how_many_labels)
 
 def label_wav(wavdir, labels, graph, input_name, output_name, how_many_labels):
   """Loads the model and labels, and runs the inference to print predictions."""
@@ -142,7 +208,7 @@ def main(_):
 	 
 #	label_wav("/tmp/speech_dataset/left/a5d485dc_nohash_0.wav","/tmp/speech_commands_train/conv_labels.txt","/tmp/my_frozen_graph.pb","wav_data:0","labels_softmax:0",3)	
 
-main(0)
+#main(0)
 """
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
